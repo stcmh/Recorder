cc3cf43da3b3269bf82f0ad5462981ac4c3e4dbf
stcmh <chlalsgur900@naver.com>
2025-03-17 18:46:45 +0900

    Update cctv_recorder.py
    final commit

--- cctv_recorder.py
+++ cctv_recorder.py
@@ -6,14 +6,13 @@ video_file = 'http://210.99.70.120:1935/live/cctv001.stream/playlist.m3u8'
 video = cv.VideoCapture(video_file)
 assert video.isOpened(), 'Cannot read the given video, ' + video_file
 
-
 fps = 30
 wait_msec = int(1000 / fps)
 is_recording = False
 out = None
 flip_horizontal = False
 overlay_image = cv.imread('button.png')
-overlay_resized = cv.resize(overlay_image, (100, 100))
+overlay_resized = cv.resize(overlay_image, (25, 25))
 
 while True:
     # Get an image from 'video' 
@@ -24,22 +23,11 @@ while True:
     frame_width = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
     frame_height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
    
-    x, y = 50, 50
-    img[y:y+overlay_resized.shape[0], x:x+overlay_resized.shape[1]] = overlay_resized     
+    x, y = 50, 50    
    
     # Show the image
     frame = int(video.get(cv.CAP_PROP_POS_FRAMES))
-        
-    # 화면에 텍스트 표시
-    info = "Video Recorder"
-    cv.putText(img, info, (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
- 
-    # 텍스트 출력: '녹화 시작!' 또는 '녹화 종료!'
-    if is_recording:
-        info = "녹화 시작"
-    else:
-        info = "녹화 종료"
-        
+    
     if flip_horizontal:
         img = cv.flip(img, 1) 
             
@@ -51,8 +39,9 @@ while True:
             out = cv.VideoWriter('output.avi', fourcc, fps, (int(video.get(3)), int(video.get(4))))
             
         out.write(img)
-       
-        
+        # 녹화 중 버튼 표시
+        img[y:y+overlay_resized.shape[0], x:x+overlay_resized.shape[1]] = overlay_resized
+   
     # 화면에 영상 출력
     cv.imshow('Video Player', img)
 
@@ -62,10 +51,10 @@ while True:
     if key == ord(' '): 
         is_recording = not is_recording
         if is_recording:
-            print("------------------------------------------------------------!")
+            print("녹화 중")
             
         else:
-            print("------------------------------------------------------------! 종료!")
+            print("녹화 종료")
             if out is not None:
                 out.release()
                 out = None
